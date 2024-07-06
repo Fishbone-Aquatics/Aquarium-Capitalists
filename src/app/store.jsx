fba19066bdfd8b8f93123098d86426bbb58cefd4
Justin Sobieski <jmksobieski@gmail.com>
2024-07-06 16:21:47 -0400

    fix(Battle) Remove battle from store.
    Remove all battle data from the redux store.

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,11 +1,9 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import playerReducer from '../features/player/playerSlice';
-import enemiesReducer from '../features/enemies/enemiesSlice';
 
 export const store = configureStore({
   reducer: {
     player: playerReducer,
-    enemies: enemiesReducer,
   },
 });
