c8e92c2c49c5eea2ae17065c2e4c6d99945acfcb
Justin Sobieski <jmksobieski@gmail.com>
2024-07-13 20:26:56 -0400

    refactor(expeditions)
    had some logic bugs, reworking the logic.

--- src/features/expeditions/expeditionSlice.js
+++ src/features/expeditions/expeditionSlice.js
@@ -15,11 +15,10 @@ const loadInitialState = () => {
           duration: 3, // seconds
           xpRange: [1, 3],
           lootDrops: [
-            { type: 'item', item: items.equipment.filter, chance: 0.33 },
-            { type: 'item', item: items.equipment.spongeFilter, chance: 0.33 },
-            { type: 'item', item: items.equipment.light, chance: 0.33 },
+            { type: 'item', item: items.equipment.filter, chance: 0.0033 },
+            { type: 'item', item: items.equipment.spongeFilter, chance: 0.0033 },
+            { type: 'item', item: items.equipment.light, chance: 0.0033 },
             { type: 'currency', amountRange: [5, 10], chance: 0.2 },
-            { type: 'currency', amountRange: [10, 20], chance: 0.1 }
           ],
           image: 'icons/expeditions/coralReef.png' 
         },
@@ -32,7 +31,6 @@ const loadInitialState = () => {
             { type: 'item', item: items.equipment.canisterFilter, chance: 0.005 },
             { type: 'item', item: items.equipment.largeFilter, chance: 0.002 },
             { type: 'currency', amountRange: [10, 20], chance: 0.2 },
-            { type: 'currency', amountRange: [20, 30], chance: 0.1 }
           ],
           image: 'icons/expeditions/shipwreck.png' 
         },
@@ -45,7 +43,6 @@ const loadInitialState = () => {
             { type: 'item', item: items.equipment.largeHeater, chance: 0.005 },
             { type: 'item', item: items.equipment.mediumHeater, chance: 0.05 },
             { type: 'currency', amountRange: [15, 25], chance: 0.2 },
-            { type: 'currency', amountRange: [25, 35], chance: 0.1 }
           ],
           image: 'icons/expeditions/trench.png' 
         },
@@ -58,7 +55,6 @@ const loadInitialState = () => {
             { type: 'item', item: items.bread, chance: 0.1 },
             { type: 'item', item: items.apple, chance: 0.05 },
             { type: 'currency', amountRange: [20, 30], chance: 0.2 },
-            { type: 'currency', amountRange: [30, 40], chance: 0.1 }
           ],
           image: 'icons/expeditions/underwaterCave.png'
         }
--- src/pages/Expeditions.jsx
+++ src/pages/Expeditions.jsx
@@ -1,114 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActiveZone, clearActiveZone } from '../features/expeditions/expeditionSlice';
 import { addXp, addItemToInventory, addCurrency } from '../features/player/playerSlice';
 import { randomNumberInRange } from '../utils/randomNumberInRange'; // Import the utility function
 import '../styles/expeditions.css'; // Import the CSS file
 
-const getRandomBubbleProperties = () => {
-  const colors = ['rgba(255, 255, 255, 0.8)', 'rgba(135, 206, 235, 0.8)'];
-  const borders = ['2px solid rgba(255, 255, 255, 0.5)', '2px solid rgba(135, 206, 235, 0.5)'];
-
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  const border = borders[Math.floor(Math.random() * borders.length)];
-
-  return { color, border };
-};
-
 const Expeditions = () => {
   const dispatch = useDispatch();
   const activeZone = useSelector((state) => state.expedition.activeZone);
   const zones = useSelector((state) => state.expedition.zones);
-  const [progress, setProgress] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
-  const [bubbles, setBubbles] = useState([]);
-  const [hoveredZone, setHoveredZone] = useState(null);
+  const intervalIdRef = useRef(null);
 
   useEffect(() => {
-    if (activeZone && !intervalId) {
-      const id = setInterval(() => {
-        setProgress(prevProgress => {
-          const zone = zones.find(zone => zone.name === activeZone);
-          const newProgress = prevProgress + 1;
-          if (newProgress >= zone.duration) {
-            // Calculate XP
-            const xp = Math.floor(Math.random() * (zone.xpRange[1] - zone.xpRange[0] + 1)) + zone.xpRange[0];
-            dispatch(addXp(xp));
+    if (intervalIdRef.current) {
+      clearInterval(intervalIdRef.current);
+      intervalIdRef.current = null;
+      console.log('Cleared existing interval in useEffect');
+    }
 
-            // Calculate loot drops
-            const totalChance = zone.lootDrops.reduce((total, drop) => total + drop.chance, 0);
-            const randomChance = Math.random() * totalChance;
+    if (activeZone) {
+      const zone = zones.find(zone => zone.name === activeZone);
+      console.log(`Starting expedition in zone: ${activeZone}`);
 
-            let cumulativeChance = 0;
-            for (let drop of zone.lootDrops) {
-              cumulativeChance += drop.chance;
-              if (randomChance <= cumulativeChance) {
-                if (drop.type === 'item') {
-                  dispatch(addItemToInventory({ item: drop.item }));
-                  console.log(`Added item: ${drop.item.name} to inventory`);
-                } else if (drop.type === 'currency') {
-                  const amount = randomNumberInRange(drop.amountRange[0], drop.amountRange[1]);
-                  dispatch(addCurrency(amount));
-                  console.log(`Added currency: ${amount}`);
-                }
-                break;
-              }
-            }
+      intervalIdRef.current = setInterval(() => {
+        console.log('Granting rewards...');
+        
+        // Calculate XP
+        const xp = Math.floor(Math.random() * (zone.xpRange[1] - zone.xpRange[0] + 1)) + zone.xpRange[0];
+        dispatch(addXp(xp));
 
-            // Delay for 1 second before resetting progress
-            setTimeout(() => {
-              setProgress(0);
-            }, 1000);
+        // Calculate loot drops
+        const totalChance = zone.lootDrops.reduce((total, drop) => total + drop.chance, 0);
+        const randomChance = Math.random() * totalChance;
 
-            return zone.duration; // Ensure it shows 100% for the delay duration
-          }
-          return newProgress;
+        let cumulativeChance = 0;
+        const selectedDrop = zone.lootDrops.find(drop => {
+          cumulativeChance += drop.chance;
+          return randomChance <= cumulativeChance;
         });
-      }, 1000);
-      setIntervalId(id);
-      console.log(`Expedition started in zone: ${activeZone}`);
+
+        if (selectedDrop) {
+          if (selectedDrop.type === 'item') {
+            dispatch(addItemToInventory({ item: selectedDrop.item }));
+            console.log(`Added item: ${selectedDrop.item.name} to inventory`);
+          } else if (selectedDrop.type === 'currency') {
+            const amount = randomNumberInRange(selectedDrop.amountRange[0], selectedDrop.amountRange[1]);
+            dispatch(addCurrency(amount));
+            console.log(`Added currency: ${amount}`);
+          }
+        }
+      }, zone.duration * 1000); // Using duration in seconds for simplicity
     }
 
     return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-        setIntervalId(null);
+      if (intervalIdRef.current) {
+        clearInterval(intervalIdRef.current);
+        intervalIdRef.current = null;
+        console.log('Interval cleared in cleanup');
       }
     };
-  }, [activeZone, intervalId, zones, dispatch]);
-
-  useEffect(() => {
-    if (hoveredZone) {
-      const interval = setInterval(() => {
-        const newBubble = {
-          id: Date.now(),
-          left: `${Math.random() * 100}%`,
-          duration: `${4 + Math.random() * 2}s`,
-          delay: `${Math.random() * 2}s`,
-          size: `${10 + Math.random() * 15}px`,
-          ...getRandomBubbleProperties(),
-        };
-        setBubbles(prevBubbles => [...prevBubbles, newBubble]);
-      }, 500);
-
-      return () => clearInterval(interval);
-    } else {
-      setBubbles([]);
-    }
-  }, [hoveredZone]);
+  }, [activeZone, zones, dispatch]);
 
   const handleStart = (zoneName) => {
+    if (intervalIdRef.current) {
+      clearInterval(intervalIdRef.current); // Clear any existing interval
+      intervalIdRef.current = null;
+      console.log('Cleared existing interval before starting a new one');
+    }
     dispatch(setActiveZone({ zoneName }));
-    setProgress(0);
+    console.log(`Expedition started in zone: ${zoneName}`);
   };
 
   const handleStop = () => {
-    if (intervalId) {
-      clearInterval(intervalId);
-      setIntervalId(null);
+    if (intervalIdRef.current) {
+      clearInterval(intervalIdRef.current);
+      intervalIdRef.current = null;
+      console.log('Interval cleared in handleStop');
     }
     dispatch(clearActiveZone());
-    setProgress(0);
     console.log('Expedition stopped.');
   };
 
@@ -121,24 +90,7 @@ const Expeditions = () => {
             key={index}
             className={`zone ${activeZone === zone.name ? 'active' : ''}`}
             style={{ backgroundImage: `url(${zone.image})` }}
-            onMouseEnter={() => setHoveredZone(zone.name)}
-            onMouseLeave={() => setHoveredZone(null)}
           >
-            {hoveredZone === zone.name && bubbles.map(bubble => (
-              <div
-                key={bubble.id}
-                className="bubble"
-                style={{
-                  left: bubble.left,
-                  animationDuration: bubble.duration,
-                  animationDelay: bubble.delay,
-                  width: bubble.size,
-                  height: bubble.size,
-                  backgroundColor: bubble.color,
-                  border: bubble.border,
-                }}
-              />
-            ))}
             <div className="header-container">
               <h2 className="heading" title={zone.name}>{zone.name}</h2>
               <div className="button-container">
@@ -151,12 +103,6 @@ const Expeditions = () => {
             </div>
             <div className="description-container">
               <p>{zone.description}</p>
-              {activeZone === zone.name && (
-                <div className="progress-bar-container">
-                  <div className="progress-bar" style={{ width: `${(progress / zone.duration) * 100}%` }}></div>
-                  <span className="progress-text">{progress}s / {zone.duration}s</span>
-                </div>
-              )}
             </div>
           </div>
         ))}
