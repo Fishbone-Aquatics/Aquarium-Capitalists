5faa371b07459e348f1a97c986e50272c20a978a
Justin Sobieski <jmksobieski@gmail.com>
2024-07-14 17:18:36 -0400

    refactor(expeditions)
    just a tweaks to drop rate for the first zone. drop system is gonna need reworked, or defined better.

--- src/features/expeditions/expeditionSlice.js
+++ src/features/expeditions/expeditionSlice.js
@@ -11,10 +11,10 @@ const initialState = {
       duration: 3, // seconds
       xpRange: [1, 3],
       lootDrops: [
-        { type: 'item', item: items.equipment.filter, chance: .9 },
+        { type: 'item', item: items.equipment.filter, chance: 0.0033 },
         { type: 'item', item: items.equipment.spongeFilter, chance: 0.0033 },
         { type: 'item', item: items.equipment.light, chance: 0.0033 },
-        //{ type: 'currency', amountRange: [5, 10], chance: 0.002 },
+        { type: 'currency', amountRange: [5, 10], chance: 0.2 },
       ],
       image: 'icons/expeditions/coralReef.png' 
     },
