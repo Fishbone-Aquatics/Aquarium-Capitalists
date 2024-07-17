a393bf9855ed2d147f81c09fe1cc831506500d48
Justin Sobieski <jmksobieski@gmail.com>
2024-07-17 16:30:49 -0400

    docs(Version): update version text
    Example work flow.

--- src/components/VersionDisplay.jsx
+++ src/components/VersionDisplay.jsx
@@ -12,7 +12,7 @@ const VersionDisplay = () => {
     fetchVersion();
   }, []);
 
-  return <div className="version-display">version: {version}</div>;
+  return <div className="version-display">version example: {version}</div>;
 };
 
 export default VersionDisplay;
